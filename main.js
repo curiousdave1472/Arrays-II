9c00c597ee0960b0dc81101656fdfd4a008ffd1a
curiousdave1472 <davidshanlay@gmail.com>
2026-06-11 23:44:56 +0000

    Nested loop

--- main.js
+++ main.js
@@ -1,1 +1,22 @@
-// Write your code below
+//Assigned the array as words
+var words = [ "Hi", "Butterfly", "Tiger", "Deer"];
+
+//Create function to find the word inside the variable "WORDS".
+function findElement(words, word){
+
+//Create the for loop to search inside the array.
+
+for(var i=0; i < words.length; i++) {
+
+//Set the condition of the search
+    if (words[i] === word)
+
+//Set the return values
+    return true;
+}
+    return false;
+};
+
+console.log(findElement(words, "Leo"));
+
+console.log(findElement(words, "Deer"))
